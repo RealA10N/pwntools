15bdd4a766a07c3b4222d6d0aea55f706d151d72
Arusekk <floss@arusekk.pl>
2025-03-24 13:00:20 +0100

    Release 4.14.1

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ if not os.path.exists(PythonH):
     sys.exit(-1)
 
 setup(
-    version              = '4.14.0',
+    version              = '4.14.1',
     data_files           = [('pwntools-doc',
                              glob.glob('*.md') + glob.glob('*.txt')),
                             ],
